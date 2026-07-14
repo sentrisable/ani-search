c47a38b3b155d448d05143c96540497a17cd0c91
sentri <sentrisable@gmail.com>
2026-07-14 15:47:20 -0500

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,153 @@
+use std::{
+    io::{stdout, Write, Read},
+    sync::mpsc::{Sender, Receiver},
+    error::Error,
+    process::Command
+
+};
+
+use json::parse;
+use serde_derive::Deserialize;
+use serde_derive::Serialize;
+use serde_json::Value;
+use curl::easy::{Handler, Easy2, WriteError, ReadError, List};
+
+#[derive(PartialEq, Debug)]
+pub enum Translation{
+    Sub,
+    Dub,
+    Raw
+}
+
+
+struct PostHandler{
+    upload_data: String,
+    response_data: Vec<u8>
+}
+
+impl Handler for PostHandler{
+    fn read(&mut self, data: &mut [u8])->Result<usize, ReadError>{
+        let size = self.upload_data.as_bytes().read(data).map_err(|_| ReadError::Abort)?;
+        Ok(size)    
+    }
+
+    fn write(&mut self, data: &[u8]) ->Result <usize, WriteError>{
+        self.response_data.extend_from_slice(data);
+        Ok(data.len())
+    }
+}
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Root {
+    pub data: Data,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Data {
+    pub shows: Shows,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Shows {
+    pub edges: Vec<Edge>,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Edge {
+    pub available_episodes: AvailableEpisodes,
+    pub id: String,
+    pub name: String,
+    pub typename: String,
+}
+
+#[derive(Default, Debug, Clone, PartialEq, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct AvailableEpisodes {
+    pub dub: Option<i64>,
+    pub raw: Option<i64>,
+    pub sub: Option<i64>,
+}
+
+fn post_curl(payload: String, show_id: Option<&String>) ->Result<String, Box<dyn std::error::Error>>{
+    
+    let ref_url = "https://youtu-chan.com";
+    let api_base =  "allanime.day";
+    let api_url = format!("https://api.{}", api_base);
+
+    let handler = PostHandler{
+        upload_data: payload.clone(),
+        response_data: Vec::new()
+    };
+    let mut handle = Easy2::new(handler);
+    handle.referer(ref_url)?;
+
+    let mut headers = List::new();
+    headers.append("Content-Type: application/json")?;
+    handle.http_headers(headers)?;
+    
+    handle.post(true)?;
+    handle.post_field_size(payload.clone().as_bytes().len() as u64)?;
+    
+    
+    handle.url(&format!("{api_url}/api/"))?;
+
+    handle.useragent("Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:150.0) Gecko/20100101 Firefox/150.0")?;
+
+    handle.verbose(true)?;
+
+    handle.perform()?;
+    
+
+    let contents = handle.get_ref();
+    let response =&std::str::from_utf8(&contents.response_data)?.replace("_", "");
+    Ok(response.clone())
+}
+
+
+pub fn get_shows() -> Result<Vec<Edge>, Box<dyn std::error::Error>>{
+
+    let search_gql="query( $search: SearchInput $limit: Int $page: Int $translationType: VaildTranslationTypeEnumType $countryOrigin: VaildCountryOriginEnumType ) { shows( search: $search limit: $limit page: $page translationType: $translationType countryOrigin: $countryOrigin ) { edges { _id name availableEpisodes __typename } }}";
+
+    let payload = format!("{{\"variables\":{{\"search\":{{\"allowAdult\":true,\"allowUnknown\":false,\"query\":\"yani\"}},\"limit\":40,\"page\":1,\"translationType\":\"sub\",\"countryOrigin\":\"ALL\"}},\"query\":\"{search_gql}\"}}");
+
+    let response = post_curl(payload, None)?;
+    //let json: Value = serde_json::from_str(response)?; 
+    //println!("Response Body: {json:?}");
+
+    let data:Root = serde_json::from_str(&response)?;
+    
+    Ok(data.data.shows.edges)
+}
+
+pub fn get_episode_list(show_id: &String) -> Result<(), Box<dyn std::error::Error>>{
+    let episode_list_gql = "query ($showId: String!) { show( _id: $showId ) { _id availableEpisodesDetail }}";
+
+    let payload = format!("{{\"variables\":{{\"showId\":\"{show_id}\"}},\"query\":\"{episode_list_gql}\"}}");
+
+    let response = post_curl(payload, Some(show_id))?;
+    let json:Value = serde_json::from_str(&response)?;
+    println!("{:?}", json["data"]);
+
+    let data:Edge = serde_json::from_str(&json["data"]["show"].to_string().replace("Detail", ""))?;
+    println!("{:?}", data);
+
+
+    Ok(())
+
+}
+
+pub fn get_episode_url(show_id: &String, translation_type: Translation,  episode_num: i16){
+    let episode_embed_gql="query ($showId: String!, $translationType: VaildTranslationTypeEnumType!, $episodeString: String!) { episode( showId: $showId translationType: $translationType episodeString: $episodeString ) { episodeString sourceUrls }}";
+    let mut translation = String::new();
+    match translation_type{
+        Translation::Sub => translation = "sub".to_string(),
+        Translation::Dub => translation = "dub".to_string(),
+        Translation::Raw => translation = "raw".to_string(),
+    }
+    println!("{translation}");
+    //let query_vars=format!("{{\"showId\":\"{}\",\"translationType\":\"{}\",\"episodeString\":\"{episode_num}\"}}", show_id, translation_type, );
+    //let query_ext="{\"persistedQuery\":{\"version\":1,\"sha256Hash\":\"$allanime_query_hash\"}, \"aaReq\":\"$(get_aa_req)\"}";
+}
---
+++ src/main.rs
@@ -0,0 +1,128 @@
+use std::{time::Duration, sync::{mpsc::{Receiver, Sender}, Arc, Mutex}, task};
+
+use eframe::egui;
+
+use egui::{ComboBox};
+use tokio::runtime::Runtime;
+
+
+use ani_search::{
+    get_shows,
+    get_episode_list,
+    Translation,
+    Edge,
+
+    
+};
+fn main() {
+    let rt = Runtime::new().expect("Unable to create Runtime");
+    let _enter = rt.enter();
+    
+    std::thread::spawn(move || {
+        rt.block_on(async {
+            loop{
+                tokio::time::sleep(Duration::from_secs(3600)).await;
+            }
+        })
+    });
+
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native("AniSearch", native_options, Box::new(|cc| Ok(Box::new(Main::new(cc)))));
+}
+
+
+
+struct Main{
+    anime: String,
+    translation: Translation,
+    shows: Vec<Edge>,
+
+    tx: Sender<String>,
+    rx: Receiver<String>,
+}
+
+impl Default for Main{
+    fn default() -> Self {
+        let (tx, rx) = std::sync::mpsc::channel();
+        Self {
+            anime: Default::default(),
+            translation: Translation::Sub,
+            shows: Vec::new(),
+            tx, 
+            rx 
+        }
+    }
+}
+
+impl Main{
+    fn new(cc: &eframe::CreationContext<'_>)->Self{
+        Self::default()
+    }
+}
+
+impl eframe::App for Main{
+    fn ui(&mut self, ui: &mut egui::Ui, frame: &mut eframe::Frame){
+        egui::CentralPanel::default().show(ui, |ui|{
+            ui.horizontal(|ui|{
+                ui.add(egui::TextEdit::singleline(&mut self.anime).hint_text("Search for show..."));
+                ComboBox::new("translation", "Translation Type").selected_text(format!("{:?}", self.translation)).show_ui(ui, |ui|{
+                        ui.selectable_value(&mut self.translation, Translation::Sub, "Sub");
+                        ui.selectable_value(&mut self.translation, Translation::Dub, "Dub");
+                        ui.selectable_value(&mut self.translation, Translation::Raw, "Raw");
+                    }
+                );
+                
+                if ui.button("Search").clicked(){
+                    if let Ok(shows) = get_shows(){
+                        self.shows = shows.clone();
+                    }
+                }
+            });
+
+            if !self.shows.is_empty(){
+                let mut selected_states = vec![false; self.shows.clone().len()];
+                for (i,show) in self.shows.iter().enumerate()
+                {
+                    let mut is_selected = selected_states[i];
+                    ui.horizontal(|ui|{
+                        // ui.label(&show.id);
+                        // ui.separator();
+
+                        if ui.selectable_label(is_selected, &show.name).clicked(){
+                            get_episode_list(&show.id);
+                        };
+                        ui.separator();
+                        ui.label("Number of episodes :");
+                        match self.translation{
+                            Translation::Sub => {
+                                if let Some(sub) = show.available_episodes.sub{
+                                    ui.label(sub.to_string());
+                                } else {
+                                    ui.label("No available episodes for that translation type");
+                                }
+                            },
+                            Translation::Dub => {
+                                if let Some(dub) = show.available_episodes.dub{
+                                    ui.label(dub.to_string());
+                                } else {
+                                    ui.label("No available episodes for that translation type");
+                                }
+                            },
+                            Translation::Raw => {
+                                if let Some(raw) = show.available_episodes.raw{
+                                    ui.label(raw.to_string());
+                                } else {
+                                    ui.label("No available episodes for that translation type");
+                                }
+                            }
+
+                        }
+                    });
+                    
+                }
+            }
+
+            
+        });
+    }
+}
